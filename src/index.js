e5a688909e0a331fde37ab1c882351af4262cceb
aytsaN <aytsan.shamal@gmail.com>
2018-09-26 13:32:33 +0300

    improve makeExchange

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    coins= {};
+    let coins = {};
+    let coinValue = Math.floor(currency / 50);
 
     if (currency < 1) {
         return coins;
     }
     if (currency > 9999) {
-        coins.error='You are rich, my friend! We don\'t have so much coins for exchange';
+        coins.error = 'You are rich, my friend! We don\'t have so much coins for exchange';
         return coins;
-    }
-    else {
-            var h = Math.floor(currency / 50);
-            if (h > 0) {
-                coins.H = h;
-                currency -= h * 50;
-            }
-            var q = Math.floor(currency / 25);
-            if (q > 0) {
-                coins.Q = q;
-                currency -= q * 25;
-            }
-            var d = Math.floor(currency / 10);
-            if (d > 0) {
-                coins.D = d;
-                currency -= d * 10;
-            }
-            var n = Math.floor(currency / 5);
-            if (n > 0) {
-                coins.N = n;
-                currency -= n * 5;
-            }
-            if(currency!=0) {coins.P = currency;}
-            return coins;
+    } else {
+        if (coinValue > 0) {
+            coins.H = coinValue;
+            currency -= coinValue * 50;
+        }
+        coinValue = Math.floor(currency / 25);
+        if (coinValue > 0) {
+            coins.Q = coinValue;
+            currency -= coinValue * 25;
+        }
+        coinValue = Math.floor(currency / 10);
+        if (coinValue > 0) {
+            coins.D = coinValue;
+            currency -= coinValue * 10;
+        }
+        coinValue = Math.floor(currency / 5);
+        if (coinValue > 0) {
+            coins.N = coinValue;
+            currency -= coinValue * 5;
+        }
+        if (currency != 0) { coins.P = currency; }
+        return coins;
     }
 }
